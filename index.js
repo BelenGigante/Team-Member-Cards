c50b2a3c69527d3f295a6a31d1960d546cdde64b
BelenGigante <vinn_a@hotmail.com>
2023-01-03 23:11:03 -0500

    generating html

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateHTML = ({name,id,email,position,office,school,github})
 
 
 inquirer.prompt([
@@ -10,19 +11,19 @@ inquirer.prompt([
         type: 'input',
         message: 'Enter name: ',
         name: 'name',
-        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter title to continue' } }
+        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter name to continue' } }
     },
     {
         type: 'input',
         message: 'Enter Id: ',
         name: 'id',
-        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter title to continue' } }
+        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter Id to continue' } }
     },
     {
         type: 'input',
         message: 'Enter e-mail: ',
         name: 'email',
-        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter title to continue' } }
+        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter e-mail to continue' } }
     },
     {
         type: 'list',
@@ -38,20 +39,20 @@ inquirer.prompt([
             type: 'input',
             message: 'Enter office number: ',
             name: 'office',
-            validate: (notBlank) => { if (notBlank) { return true } else { return 'enter title to continue' } }
+            validate: (notBlank) => { if (notBlank) { return true } else { return 'enter office number to continue' } }
         })
     } if (`${data.position}` === 'Intern') {
         inquirer.prompt({
             type: 'input',
             message: 'Enter school: ',
             name: 'school',
-            validate: (notBlank) => { if (notBlank) { return true } else { return 'enter title to continue' } }
+            validate: (notBlank) => { if (notBlank) { return true } else { return 'enter school to continue' } }
         })
     
     }if (`${data.position}` === 'Engineer') {
         inquirer.prompt({type: 'input',
         message: 'Enter GitHub username: ',
         name: 'github',
-        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter title to continue' } }})
+        validate: (notBlank) => { if (notBlank) { return true } else { return 'enter username to continue' } }})
     }
 })
