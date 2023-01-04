8e996ee857ce09e07cb3e5b6afe8e2d67036c880
BelenGigante <vinn_a@hotmail.com>
2023-01-04 01:54:06 -0500

    test employee trial and error

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,22 @@
+const { TestScheduler } = require("jest");
 const Employee = require("../lib/Employee");
 
+
 describe("Employee", () => {
+it('should return an object containing basic employee information when called with the "new" keyword', () => {
+    const employee = new Employee('2525','belen','dinno@gjgj.com');
+    expect(employee.id).toBe(2525);
+    expect(employee.name).toBe("belen");
+    expect(employee.email).toBe("dinno@gjgj.com");
+});
+test('confirm the getId method works',()=>{
+expect(employee.getId()).toBe('2525');
+});
+test('confirm the getName method works',()=>{
+expect(employee.getName()).toBe('belen');
+});
+test('confirm the getEmail method works',()=>{
+expect(employee.getEmail()).toBe('dinno@gjgj.com');
+});
 
 });
--- index.js
+++ index.js
@@ -3,7 +3,78 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHTML = ({name,id,email,position,office,school,github})
+
+
+const generateHTML = ({ name,id,position, email, github,school,office }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ENTER PAGE TITLE HERE</title>
+  <!-- Minified version -->
+  <style>
+    @import "https://cdn.simplecss.org/simple.min.css";
+
+    main {
+      display: grid;
+      grid-column: 1/-1;
+      justify-items: center;
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 1rem;
+      max-width: 1140px;
+      margin: auto;
+    }
+
+    @media screen and (max-width: 1140px) {
+      main {
+        grid-template-columns: 1fr 1fr;
+      }
+    }
+    @media screen and (max-width: 720px) {
+      main {
+        grid-template-columns: 1fr;
+      }
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>My Team</h1>
+  </header>
+  <main>
+    <article>
+      <h2>${position}</h2>
+      <ul>
+        <li>Name: ${name}</li>
+        <li>ID: ${id}</li>
+        <li>Email: ${email}</li>
+        <li>Office Number: ${office}</li>
+      </ul>
+    </article>
+    <article>
+      <h2>position</h2>
+      <ul>
+        <li>ID: 2</li>
+        <li>Email: engineer@email.com</li>
+        <li>GitHub: ${github}<a href="#github">person</a></li>
+      </ul>
+    </article>
+    <article>
+      <h2>Intern</h2>
+      <ul>
+        <li>ID: 3</li>
+        <li>Email: intern@email.com</li>
+        <li>School: ${school}</li>
+      </ul>
+    </article>
+  </main>
+  <footer>
+    &copy; 2022-2023
+  </footer>
+</body>
+</html>`;
 
 
 inquirer.prompt([
@@ -32,8 +103,10 @@ inquirer.prompt([
         name: 'position',
         
     },
+    
 
 ]).then((data) => {
+    
     if (`${data.position}` === 'Manager') {
         inquirer.prompt({
             type: 'input',
@@ -55,4 +128,8 @@ inquirer.prompt([
         name: 'github',
         validate: (notBlank) => { if (notBlank) { return true } else { return 'enter username to continue' } }})
     }
+    const htmlPageContent = generateHTML(data);
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
 })
