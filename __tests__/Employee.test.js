11fcd7c51ceb694c30d17b12e7799b14fba0f0d8
BelenGigante <vinn_a@hotmail.com>
2023-01-04 17:43:29 -0500

    corrected test with TAs help

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,22 +1,28 @@
-const { TestScheduler } = require("jest");
 const Employee = require("../lib/Employee");
 
+const id = 456;
+const name = "fhf";
+const email = "th";
 
 describe("Employee", () => {
-it('should return an object containing basic employee information when called with the "new" keyword', () => {
-    const employee = new Employee('2525','belen','dinno@gjgj.com');
-    expect(employee.id).toBe(2525);
-    expect(employee.name).toBe("belen");
-    expect(employee.email).toBe("dinno@gjgj.com");
-});
-test('confirm the getId method works',()=>{
-expect(employee.getId()).toBe('2525');
-});
-test('confirm the getName method works',()=>{
-expect(employee.getName()).toBe('belen');
-});
-test('confirm the getEmail method works',()=>{
-expect(employee.getEmail()).toBe('dinno@gjgj.com');
-});
+    it('should return an object containing basic employee information when called with the "new" keyword', () => {
+
+        const employee = new Employee(id, name, email);
+        expect(employee.id).toBe(456);
+        expect(employee.name).toBe("fhf");
+        expect(employee.email).toBe("th");
+    });
+    it('take in an id with getId method', () => {
+        const employee = new Employee(id, name, email);
+        expect(employee.getId()).toBe(456);
+    });
+    it('take in a name with getName method', () => {
+        const employee = new Employee(id, name, email);
+        expect(employee.getName()).toBe('fhf');
+    });
+    it('take in an email with getEmail method', () => {
+        const employee = new Employee(id, name, email);
+        expect(employee.getEmail()).toBe('th');
+    });
 
 });
